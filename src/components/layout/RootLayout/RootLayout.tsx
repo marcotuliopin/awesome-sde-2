7ef88f829797645defa780d37f190253cd647f75
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-21 19:35:32 -0300

    reorganize code

--- src/components/layout/RootLayout/RootLayout.tsx
+++ src/components/layout/RootLayout/RootLayout.tsx
@@ -1,17 +1,20 @@
-import { Outlet } from 'react-router-dom';
-import { Header } from '@components/layout/Header';
-import { useTheme } from '@/contexts/ThemeContext';
-
+import { Outlet } from "react-router-dom";
+import { Header } from "@components/layout/Header";
+import { useTheme } from "@/contexts/ThemeContext";
 
 export const RootLayout = () => {
-  const { theme } = useTheme();
-  
-  return (
-    <div className={`${theme === "dark" ? "dark" : ""} transition-colors duration-500 dark:bg-gray-900 bg-gray-100 min-h-screen`}>
-      <Header />
-      <main className="container mx-auto px-4 py-8">
-        <Outlet />
-      </main>
-    </div>
-  );
+    const { theme } = useTheme();
+
+    return (
+        <div
+            className={`${
+                theme === "dark" ? "dark" : ""
+            } transition-colors duration-500 dark:bg-gray-900 bg-gray-100 min-h-screen`}
+        >
+            <Header />
+            <main className="container mx-auto px-4 py-8">
+                <Outlet />
+            </main>
+        </div>
+    );
 };
