ffbee61741cb6d1f20de7adece7b7a9635306341
Raul Araju <raularaju@uol.com.br>
2025-05-25 17:19:43 -0300

    feat: inicio do backend

---
+++ backend/src/domains/product/controllers/index.js
@@ -0,0 +1,36 @@
+const Router = require("express").Router();
+const ProductService = require("../services/productService");
+const httpsStatusCodes = require("../../../utils/constants/httpStatusCodes");
+
+Router.post("/", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const product = await ProductService.create(req.body);
+    console.log("p: ", product);
+    res.status(httpsStatusCodes.ACCEPTED).send(product);
+  } catch (error) {
+    next(error);
+  }
+});
+
+Router.put("/:id", async (req, res, next) => {
+  try {
+    await ProductService.update(req.params.id, req.body);
+    res
+      .status(httpsStatusCodes.ACCEPTED)
+      .send("Produto atualizado com sucesso");
+  } catch (error) {
+    next(error);
+  }
+});
+
+Router.get("/", async (req, res, next) => {
+  try {
+    all_products = await ProductService.getall();
+    res.status(httpsStatusCodes.ACCEPTED).send(all_products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = Router;
---
+++ backend/src/domains/user/controllers/index.js
@@ -0,0 +1,39 @@
+const Router = require("express").Router();
+const {
+  notLoggedIn,
+  jwtMiddleware,
+  loginMiddleware,
+} = require("../../../middlewares/auth");
+const httpStatusCodes = require("../../../utils/constants/httpStatusCodes");
+const UserService = require("../services/userService");
+
+Router.post("/", async (req, res, next) => {
+  try {
+    await UserService.create(req.body);
+    res.status(httpStatusCodes.ACCEPTED).send("Conta criada com sucesso");
+  } catch (error) {
+    next(error);
+  }
+});
+
+Router.post("/login", notLoggedIn, loginMiddleware);
+
+Router.post("/logout", jwtMiddleware, async (req, res, next) => {
+  try {
+    res.clearCookie("jwt");
+    res.status(httpStatusCodes.NO_CONTENT).send("Deslogado com sucesso");
+  } catch (error) {
+    next(error);
+  }
+});
+
+Router.post("/", async (req, res, next) => {
+  try {
+    await UserService.create(req.body);
+    res.status(httpsStatusCodes.ACCEPTED).send("Usuário criado com sucesso");
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = Router;
