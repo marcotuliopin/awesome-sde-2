0973430f1dc16e76ba8f93e7c03dfc8af5ff399c
Raul Araju <raularaju@uol.com.br>
2025-05-27 15:31:43 -0300

    updating imports

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,4 +1,4 @@
-import { Product } from "../types/product";
+import type { Product } from "../types/product";
 
 
 // TODO: Use environment variables for API URL
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { User } from "../types/user";
+import type { User } from "../types/user";
 
 
 // TODO: Use environment variables for API URL
