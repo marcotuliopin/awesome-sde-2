0cf4d64cae8b7bb62ad3df14d46ebb364eef4d27
Marco Túlio P. T. Tristão <marcotuliopin@gmail.com>
2025-05-31 17:34:37 -0300

    fix main

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import AuthWrapper from '@/components/ui/AuthWrapper/AuthWrapper'; // ou caminho relativo
-import { RootLayout } from '@/components/layout/RootLayout';
-import { Root } from '@/routes/root/Root';
-import { Login } from '@/routes/login/Login';
-import { Register } from '@/routes/register/Register';
-import './index.css';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RootLayout } from "@/components/layout/RootLayout";
+import { Root } from "@/routes/root/Root";
+import { Login } from "@/routes/login/Login";
+import { Register } from "@/routes/register/Register";
+import "./index.css";
+import { AuthWrapper } from "./components/ui/AuthWrapper";
+import { ThemeProvider } from "./contexts/theme.context";
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    children: [
-     { index: true, element: <Root /> },
-     {
-       index: true,
-       element: (
-         <AuthWrapper>
-           <Root />
-         </AuthWrapper>
-       ),
-     },
-      { path: '/login', element: <Login /> },
-      { path: '/register', element: <Register /> },
-    ],
-  },
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            {
+                index: true,
+                element: (
+                    <AuthWrapper>
+                        <Root />
+                    </AuthWrapper>
+                ),
+            },
+            { path: "/login", element: <Login /> },
+            { path: "/register", element: <Register /> },
+        ],
+    },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <StrictMode>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </StrictMode>
 );
